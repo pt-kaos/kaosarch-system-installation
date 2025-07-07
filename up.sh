42845de70e1b95c59250e14bbfee98f544ee44f1
Pedro Teodoro <pedro_teodoro@hotmail.com>
2025-07-07 19:24:07 +0100

    update

--- up.sh
+++ up.sh
@@ -1,39 +1,7 @@
 #!/bin/bash
-set -eo pipefail
-##################################################################################################################
-# Author    : Erik Dubois
-# Website   : https://www.erikdubois.be
-# Youtube   : https://youtube.com/erikdubois
-# Github    : https://github.com/erikdubois
-# Github    : https://github.com/kirodubes
-# Github    : https://github.com/buildra
-# SF        : https://sourceforge.net/projects/kiro/files/
-##################################################################################################################
-#
-#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
-#
-##################################################################################################################
-#tput setaf 0 = black
-#tput setaf 1 = red
-#tput setaf 2 = green
-#tput setaf 3 = yellow
-#tput setaf 4 = dark blue
-#tput setaf 5 = purple
-#tput setaf 6 = cyan
-#tput setaf 7 = gray
-#tput setaf 8 = light blue
-##################################################################################################################
+#set -e
 
-# variables and functions
 workdir=$(pwd)
-dir="calamares-3.3.14.r39.g274996f"
-source=" /home/pedro/Programing/ArchISOs/KaosArch/kaosarch-pkgbuild/"
-destiny="/home/pedro/Programing/ArchISOs/KaosArch/kaosarch-calamares-config/etc/calamares/pkgbuild/"
-
-##################################################################################################################
-
-rm -r $destiny*
-cp -r $source$dir/* $destiny
 
 # Below command will backup everything inside the project folder
 git add --all .
@@ -44,8 +12,7 @@ git commit -m "update"
 
 # Push the local files to github
 
-branch=$(git rev-parse --abbrev-ref HEAD)
-git push -u origin "$branch"
+git push -u origin main
 
 echo
 tput setaf 6
