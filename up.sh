29a76e1ea488e5c79b1806ed8d9c3022e4c0dbf9
Pedro Teodoro <pedro_teodoro@hotmail.com>
2025-09-02 01:37:38 +0100

    New update

--- up.sh
+++ up.sh
@@ -4,23 +4,50 @@ set -eo pipefail
 # Author    : Pedro Teodoro
 # Github    : https://github.com/pt-kaos
 ##################################################################################################################
-#set -e
+#tput setaf Colors:
+#     0 = black
+#     1 = red
+#     2 = green
+#     3 = yellow
+#     4 = darkblue
+#     5 = purple
+#     6 = cyan
+#     7 = gray
+#     8 = lightblue
+##################################################################################################################
 
-workdir=$(pwd)
-
-# Below command will backup everything inside the project folder
-git add --all .
-
-# Committing to the local repository with a message containing the time details and commit text
-git commit -m "update"
-
-# Push the local files to github
-git push -u origin main
-
-echo
 tput setaf 6
-echo "##############################################################"
-echo "###################  $(basename $0) done"
-echo "##############################################################"
+echo "=============================================================="
+echo "                    Updating $(basename $(pwd))"
+echo "=============================================================="
 tput sgr0
-echo
+
+message="New update"
+
+while getopts ":m:" opt; do
+  case ${opt} in
+    m )
+      if [[ -n "$OPTARG" ]]; then
+        message="$OPTARG"
+      fi
+      ;;
+    \? )
+      echo "Usage: $0 [-m <message>]"
+      exit 1
+      ;;
+  esac
+done
+
+echo "====> git update <===="
+
+git add --all .                           # Add every change to be pushed to git
+git commit -m "$message"                  # Commit to local repository with a default message
+branch=$(git rev-parse --abbrev-ref HEAD) # Get branch name origin or master
+git push -u origin "$branch"              # Push the local files to github
+
+tput setaf 6
+echo "=============================================================="
+echo "                    $(basename $0) done"
+echo "=============================================================="
+tput sgr0
+
